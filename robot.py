1788022719d4a8cb43ba943281a39587cdc71442
tthivvyan <108170833+tthivvyan@users.noreply.github.com>
2023-06-21 05:38:33 +0530

    Add files via upload

--- robot.py
+++ robot.py
@@ -15,34 +15,34 @@ def rectangle(horizontal,vertical,color):
 
 t.penup()
 t.speed('slow')
-t.bgcolor('Dodger blue')
+t.bgcolor('peach puff')
 
 #feet
 t.goto(-100, -150)
-rectangle(50,20,'blue')
+rectangle(50,20,'red')
 t.goto(-30,-150)
 rectangle(50,20,'blue')
 
 #legs
 t.goto(-25, -50)
-rectangle(15,100,'grey')
+rectangle(15,100,'black')
 t.goto(-55,-50)
 rectangle(-15,100,'grey')
 
 #body
 t.goto(-90,100)
-rectangle(100,150,'red')
+rectangle(100,150,'goldenrod')
 
 #arms
 t.goto(-150, 70)
-rectangle(60,15,'grey')
+rectangle(60,15,'hotpink')
 t.goto(-150,110)
 rectangle(15,40,'grey')
 
 t.goto(10, 70)
-rectangle(60,15,'grey')
+rectangle(60,15,'thistle')
 t.goto(55,110)
-rectangle(15,40,'grey')
+rectangle(15,40,'black')
 
 #neck
 t.goto(-50,120)
@@ -54,15 +54,14 @@ rectangle(80,50,'red')
 
 #eyes
 t.goto(-60, 160)
-rectangle(30,10,'white')
+rectangle(30,10,'navy')
 t.goto(-55,155)
 rectangle(5,5,'black')
-t.goto(-45,155)
-rectangle(5,5,'black')
+t.goto(-40,155)
+rectangle(5,5,'lawngreen')
 
 #mouth
 t.goto(-65,135)
-
 rectangle(40,5,'black')
 
 t.hideturtle()
